b90be3d346116158b106d292e79cbeaa4e2a34fa
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2021-12-01 17:27:11 +0330

    remove personal file

--- configs.py
+++
@@ -1,5 +0,0 @@
-TOKEN_URL = "https://gw.ebcom.ir/token"
-CLIENT_AUTH_URL = "https://gw.ebcom.ir/service/auth/clientauth/client/login"
-BILL_INQUIRY_URL = "https://gw.ebcom.ir/service/payment/bill/v1.0/{bill_id}/{bill_type}"
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
