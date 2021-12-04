430b05b1768c43ac00b03c88625e28d92583b476
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2021-12-04 09:07:56 +0330

    change classes to func

--- authorization.py
+++ authorization.py
@@ -1,4 +1,3 @@
-import base64
 import httpx
 import json
 from typing import Literal
@@ -6,6 +5,7 @@ from models import (
     AccessToken,
     ClientToken
 )
+from utils import username_and_password_to_b64_token
 
 from configs import TOKEN_URL, CLIENT_AUTH_URL
 
@@ -13,53 +13,34 @@ CONSUMER_KEY = "Y365qfDj6OZ0rzpTwckKux25HSsa"
 CONSUMER_PASSWORD = "k4EyWgYjXdvbcnbpvzQcoJWMmFYa"
 
 
-class AccessTokenManager:
-    def __init__(self, consumer_key: str, consumer_password: str, grant_type: str = 'client_credentials'):
-        self.grant_type = grant_type
-        self.consumer_key = consumer_key
-        self.consumer_password = consumer_password
-        self.b64_token = self.to_b64_token(consumer_key, consumer_password)
-
-    @staticmethod
-    def to_b64_token(consumer_key, consumer_password):
-        return base64.b64encode(f'{consumer_key}:{consumer_password}'.encode()).decode()
-
-    async def get_access_token(self) -> AccessToken:
-        headers = {'Authorization': f'Basic {self.b64_token}'}
-        data = {'grant_type': self.grant_type}
-        async with httpx.AsyncClient() as client:
-            resp = await client.post(url=TOKEN_URL, data=data, headers=headers)
-        resp.raise_for_status()
-        return AccessToken(**json.loads(resp.text))
-
-    def __call__(self, *args, **kwargs):
-        return self.get_access_token()
+async def async_get_access_token(
+        consumer_key: str,
+        consumer_password: str,
+        grant_type: str = 'client_credentials'
+) -> AccessToken:
+    headers = {'Authorization': f'Basic {username_and_password_to_b64_token(consumer_key, consumer_password)}'}
+    data = {'grant_type': grant_type}
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(url=TOKEN_URL, data=data, headers=headers)
+    resp.raise_for_status()
+    return AccessToken(**json.loads(resp.text))
 
 
-class ClientTokenManager:
-    def __init__(
-            self,
-            access_token: AccessToken,
-            prefer: Literal[
-                'dynamic=false',
-                'dynamic=true',
-                'dynamic=false,code=520',
-                'dynamic=true,code=520'
-            ] = 'dynamic=false'
-    ):
-        self.access_token = access_token
-        self.prefer = prefer
-
-    async def get_client_token(self) -> ClientToken:
-        headers = {
-            "Accept": "application/json",
-            "prefer": self.prefer,
-            "Authorization": f"Bearer {self.access_token.access_token}"
-        }
-        async with httpx.AsyncClient() as client:
-            resp = await client.get(url=CLIENT_AUTH_URL, headers=headers)
-        resp.raise_for_status()
-        return ClientToken(**json.loads(resp.text)['result']['data'])
-
-    def __call__(self, *args, **kwargs):
-        return self.get_client_token()
+async def async_get_client_token(
+        access_token: AccessToken,
+        prefer: Literal[
+            'dynamic=false',
+            'dynamic=true',
+            'dynamic=false,code=520',
+            'dynamic=true,code=520'
+        ] = 'dynamic=false'
+) -> ClientToken:
+    headers = {
+        "Accept": "application/json",
+        "prefer": prefer,
+        "Authorization": f"Bearer {access_token.access_token}"
+    }
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url=CLIENT_AUTH_URL, headers=headers)
+    resp.raise_for_status()
+    return ClientToken(**json.loads(resp.text)['result']['data'])
--- core.py
+++ core.py
@@ -13,52 +13,39 @@ CONSUMER_KEY = "Y365qfDj6OZ0rzpTwckKux25HSsa"
 CONSUMER_PASSWORD = "k4EyWgYjXdvbcnbpvzQcoJWMmFYa"
 
 
-class BillInquiryManager:
-    def __init__(
-            self,
-            access_token: AccessToken,
-            client_authorization: ClientToken,
-            bill_id: str,
-            bill_type: Literal[
-                "gas",
-                "electricity",
-                "water",
-                "rahvar",
-                "mobile",
-                "mobileHotbill",
-                "pstn",
-                "pstnHotbill"
-            ] = 'mobile'
-    ):
-        self.bill_type = bill_type
-        self.bill_id = bill_id
-        self.client_authorization = client_authorization
-        self.access_token = access_token
-
-    async def get_bill_inquiry(self):
-        headers = {
-            "Accept": "application/json",
-            "Authorization": f"Bearer {self.access_token.access_token}",
-            "C-Authorization": f"Bearer {self.client_authorization.token}",
-            "prefer": "dynamic=false"
-        }
-        async with httpx.AsyncClient() as client:
-            resp = await client.get(url=BILL_INQUIRY_URL.format(bill_id=self.bill_id, bill_type=self.bill_type),
-                                    headers=headers)
-        resp.raise_for_status()
-        return BillInquiry(**json.loads(resp.text)['result']['data'])
-
-    def __call__(self, *args, **kwargs):
-        return self.get_bill_inquiry()
+async def async_get_bill_inquiry(
+        access_token: AccessToken,
+        client_authorization: ClientToken,
+        bill_id: str,
+        bill_type: Literal[
+            "gas",
+            "electricity",
+            "water",
+            "rahvar",
+            "mobile",
+            "mobileHotbill",
+            "pstn",
+            "pstnHotbill"
+        ] = 'mobile'
+):
+    headers = {
+        "Accept": "application/json",
+        "Authorization": f"Bearer {access_token.access_token}",
+        "C-Authorization": f"Bearer {client_authorization.token}",
+        "prefer": "dynamic=false"
+    }
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url=BILL_INQUIRY_URL.format(bill_id=bill_id, bill_type=bill_type),
+                                headers=headers)
+    resp.raise_for_status()
+    return BillInquiry(**json.loads(resp.text)['result']['data'])
 
 
 async def main():
-    from authorization import AccessTokenManager, ClientTokenManager
-    atm = AccessTokenManager(CONSUMER_KEY, CONSUMER_PASSWORD)
-    at = await atm.get_access_token()
-    ctm = ClientTokenManager(at)
-    ct = await ctm.get_client_token()
-    print(await BillInquiryManager(at, ct, '9122222222').get_bill_inquiry())
+    from authorization import async_get_access_token, async_get_client_token
+    at = await async_get_access_token(CONSUMER_KEY, CONSUMER_PASSWORD)
+    ct = await async_get_client_token(at)
+    print(await async_get_bill_inquiry(at, ct, '9122222222'))
 
 
 if __name__ == "__main__":
