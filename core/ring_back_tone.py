08c403d53bb1b2f6afc00819b4fda78e97d38557
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2021-12-07 16:17:49 +0330

    Completing async_rbt_inquiry function

--- core/ring_back_tone.py
+++ core/ring_back_tone.py
@@ -1,12 +1,16 @@
 from decorator import request_url
 from configs import RBT_INQUIRY_URL
 from typing import Literal
+import httpx
+import json
+from models import AccessToken
 
 
 @request_url(url=RBT_INQUIRY_URL)
 async def async_rbt_inquiry(
         *,
         url: str,
+        access_token: AccessToken,
         acl_id: str,
         prefer: Literal[
             'dynamic=false',
@@ -14,5 +18,15 @@ async def async_rbt_inquiry(
             'dynamic=false,code=520',
             'dynamic=true,code=520'
         ] = 'dynamic=false',
-        x_authorization:  str):
-    pass
+        x_authorization: str) -> Literal['ACTIVE', 'INACTIVE']:
+    headers = {
+        "Accept": "application/json; charset=UTF-8",
+        "X-Authorization": x_authorization,
+        "prefer": prefer,
+        "Authorization": access_token.access_token
+    }
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url=url, headers=headers)
+    resp.raise_for_status()
+    json_response = json.loads(resp.text)
+    return json_response['result']['data']['status']
