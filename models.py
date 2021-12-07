7dd9af8b081e0901f11f17cb2ab6a82b1f9dfc07
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2021-12-07 10:26:27 +0330

    add ACL, UserLoginSession, LoggedInUser models and change datetime field form str to datetime and handle add Bearer to first of tokens and remove UTC timezone for datetimes

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, Field, root_validator
+from typing import List, Literal, Optional
+import datetime
 
 
 class AccessToken(BaseModel):
@@ -7,15 +9,61 @@ class AccessToken(BaseModel):
     token_type: str
     expires_in: int
 
+    @root_validator
+    def check_access_token(cls, values):
+        values['access_token'] = f"{values.get('token_type')} {values.get('access_token')}"
+        return values
+
 
 class ClientToken(BaseModel):
     token: str
     refreshToken: str
-    expires: str
+    expires: datetime.datetime
     type: str
+
+    @root_validator
+    def check_token(cls, values):
+        values['token'] = f"{values.get('type')} {values.get('token')}"
+        return values
+
+    @validator('expires')
+    def remove_datetime_tz(cls, value):
+        return value.replace(tzinfo=None)
 
 
 class BillInquiry(BaseModel):
     bill_id: str
     pay_id: str
     amount: int
+
+
+class ACL(BaseModel):
+    id: str
+    msisdn: str
+    sim_type: Literal['PREPAID', 'POSTPAID'] = Field(alias='simType')
+
+
+class UserLoginSession(BaseModel):
+    id: str
+    key: str
+    prime: str
+
+
+class LoggedInUser(BaseModel):
+    id: str
+    acl: List[ACL]
+    expires_in: datetime.datetime = Field(alias='expiresIn')
+    refresh_token: str = Field(alias='refreshToken')
+    session: Optional[UserLoginSession]
+    signup: bool
+    token: str
+    type: str
+
+    @root_validator
+    def check_token(cls, values):
+        values['token'] = f"{values.get('type')} {values.get('token')}"
+        return values
+
+    @validator('expires_in')
+    def remove_datetime_tz(cls, value):
+        return value.replace(tzinfo=None)
