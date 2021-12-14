4e76c0a764d7881acc64ad20596668e6a45bca87
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2021-12-14 08:43:32 +0330

    Modify request_url decorator to handle default params

--- src/mci_services/utils.py
+++ src/mci_services/utils.py
@@ -1,4 +1,14 @@
 import base64
+import inspect
+
+
+def get_default_args(func: callable) -> dict:
+    return {
+        k: v.default
+        for k, v
+        in inspect.signature(func).parameters.items()
+        if v.default is not inspect.Parameter.empty
+    }
 
 
 def username_and_password_to_b64_token(consumer_key, consumer_password, sep: str = ':'):
